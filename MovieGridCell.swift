68fd99595e3b4c9072106388e8ad4fb5ee156bc7
bokage <37666121+bokage@users.noreply.github.com>
2019-04-21 23:34:28 -0700

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Derek Ye on 4/21/19.
+//  Copyright © 2019 Derek. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
